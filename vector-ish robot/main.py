089a4635b89efc2ece39a7c019adb65eca75e1e8
Atharva <atharva.malik@gmail.com>
2024-08-05 18:42:50 +1000

    added an animation and draw function
    plus the first animation!

--- vector-ish robot/main.py
+++ vector-ish robot/main.py
@@ -12,15 +12,30 @@
 
 from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
-import framebuf
+import framebuf, time, random
+from animations import *
+from statics import *
 
 WIDTH = 128
 HEIGHT= 64
-
 i2c = I2C(0, scl=Pin(17), sda = Pin(16), freq=200000) # 8, 9 are the default values
-
 oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
+def displayImage(img, sizeX, sizeY):
+    oled.fill(0)
+    fb = framebuf.FrameBuffer(img, sizeX, sizeY, framebuf.MONO_HLSB) # This means monochrome
+    oled.blit(fb, int((WIDTH-sizeX)/2), 0)
+    oled.show()
+
+def displayAnimation(stages, sizeX, sizeY):
+    for i in stages:
+        time.sleep(0.01)
+        displayImage(i, sizeX, sizeY)
+
+
 oled.fill(0) # CLEAR SCREEN
-oled.text("Hello, World!", 0,0)
-oled.show()
+while True:
+    displayImage(EYES, 64, 64)
+    time.sleep(random.randint(1, 3))
+    displayAnimation(BLINK, 64, 64)
+    # oled.text("Hello, World!", 0,0)
